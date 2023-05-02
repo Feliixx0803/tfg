ed9793ddc566b258a39187cf6b2677d217cc0694
Marcos Notario <marcos.notario.m@gmail.com>
2023-05-02 17:56:13 +0200

    Resubida del proyecto con application.properties correcto

--- pruebaAngular/src/main/java/org/mnotario/angular/repos/EventoRepo.java
+++
@@ -1,8 +0,0 @@
-package org.mnotario.angular.repos;
-
-import org.mnotario.angular.model.Evento;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface EventoRepo extends JpaRepository<Evento, Long>{
-	Evento findEventoById(Long id);
-}
