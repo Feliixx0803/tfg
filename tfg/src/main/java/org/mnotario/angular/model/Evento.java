87232bafb9ed3805f0e18a1d319ce4e430b6c261
Feliixx0803 <felixperezi3p@gmail.com>
2023-05-04 16:06:16 +0200

    Arreglado error del postman por peticion GET y POST de inscripcion Se ha modificado las entities usuario y evento añadiendo dos anotaciones

--- tfg/src/main/java/org/mnotario/angular/model/Evento.java
+++ tfg/src/main/java/org/mnotario/angular/model/Evento.java
@@ -5,6 +5,8 @@ import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.Collection;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
@@ -12,8 +14,11 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
 
 @Entity
+@Table(name="usuario")
+@JsonIgnoreProperties({"inscripciones"})
 public class Evento implements Serializable{
 	@Id
 	@GeneratedValue(strategy = GenerationType.AUTO)
--- tfg/src/main/java/org/mnotario/angular/model/Usuario.java
+++ tfg/src/main/java/org/mnotario/angular/model/Usuario.java
@@ -4,6 +4,8 @@ import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.Collection;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
@@ -11,8 +13,11 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
 
 @Entity
+@Table(name="usuario")
+@JsonIgnoreProperties({"inscripciones"})
 public class Usuario implements Serializable {
 	@Id
 	@GeneratedValue(strategy = GenerationType.AUTO)
