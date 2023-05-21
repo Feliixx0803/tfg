ee7cd54eea908bd102cb211973a40e103d35b08d
Feliixx0803 <felixperezi3p@gmail.com>
2023-05-21 15:07:15 +0200

    + Creada relacion usuario-rol en front: + Creados modelos y servicios correspondientes y reestructurado el componente.

--- tfg/src/main/java/org/mnotario/angular/controllers/RolController.java
+++ tfg/src/main/java/org/mnotario/angular/controllers/RolController.java
@@ -6,6 +6,7 @@ import org.mnotario.angular.model.Rol;
 import org.mnotario.angular.services.RolService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -17,6 +18,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/rol")
+@CrossOrigin(origins = "http://localhost:4200")
 public class RolController {
 
 	private final RolService rolService;
