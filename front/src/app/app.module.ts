d489df2cc317320a89370a3255940db0aa18f2fe
Feliixx0803 <felixperezi3p@gmail.com>
2023-05-21 15:05:03 +0200

    + Creada relacion usuario-rol en front: + Creados modelos y servicios correspondientes y reestructurado el componente.

--- front/src/app/app.module.ts
+++ front/src/app/app.module.ts
@@ -3,19 +3,21 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { UsuarioComponent } from './Components/usuario/usuario.component';
+import { UsuarioComponent } from './components/usuario/usuario.component';
 
 import { HttpClientModule } from '@angular/common/http';
 import { PruebaLoginComponent } from './prueba-login/prueba-login.component';
 import { FormsModule } from '@angular/forms';
 import { PruebaRegistroComponent } from './prueba-registro/prueba-registro.component';
+import { RolComponent } from './components/rol/rol.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     UsuarioComponent,
     PruebaLoginComponent,
-    PruebaRegistroComponent
+    PruebaRegistroComponent,
+    RolComponent
   ],
   imports: [
     BrowserModule,
---
+++ front/src/app/components/usuario/usuario.component.ts
@@ -0,0 +1,26 @@
+import { Component } from '@angular/core';
+import {UsuarioModel} from "../../models/usuario/usuario-model";
+import {UsuarioServiceService} from "../../services/usuario/usuario-service.service";
+
+@Component({
+  selector: 'usuario',
+  templateUrl: './usuario.component.html',
+  styleUrls: ['./usuario.component.scss']
+})
+export class UsuarioComponent {
+
+  usuarios :UsuarioModel[];
+
+  constructor(private usuarioServicio : UsuarioServiceService) { }
+
+  ngOnInit() {
+    this.getAllUsuarios()
+  }
+
+  //Recibir todos los empleados:
+  private getAllUsuarios(){
+    this.usuarioServicio.getAllUsuarios().subscribe(usuario =>{
+      this.usuarios = usuario;
+    })
+  }
+}
