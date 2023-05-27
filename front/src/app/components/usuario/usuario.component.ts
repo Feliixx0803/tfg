52788de4820acf1c33a095bfb2c5712b6f774611
Feliixx0803 <felixperezi3p@gmail.com>
2023-05-27 19:05:04 +0200

    + Creado CRUD primitivo en usuario.

--- front/src/app/components/usuario/usuario.component.ts
+++ front/src/app/components/usuario/usuario.component.ts
@@ -1,6 +1,7 @@
 import {Component, OnInit} from '@angular/core';
 import {UsuarioModel} from "../../models/usuario/usuario-model";
 import {UsuarioServiceService} from "../../services/usuario/usuario-service.service";
+import {RolModel} from "../../models/rol/rol-model";
 
 @Component({
   selector: 'usuario',
@@ -10,11 +11,12 @@ import {UsuarioServiceService} from "../../services/usuario/usuario-service.serv
 export class UsuarioComponent implements OnInit{
 
   usuarios :UsuarioModel[];
+  nuevoUsuario : UsuarioModel;
 
   constructor(private usuarioServicio : UsuarioServiceService) { }
 
   ngOnInit() {
-    this.getAllUsuarios()
+    this.getAllUsuarios();
   }
 
   //Recibir todos los empleados:
@@ -22,5 +24,17 @@ export class UsuarioComponent implements OnInit{
     this.usuarioServicio.getAllUsuarios().subscribe(usuario =>{
       this.usuarios = usuario;
     })
+  }
+
+  private createUsuario(){
+    this.usuarioServicio.createUser(this.nuevoUsuario).subscribe();
+  }
+
+  /*private updateUsuario(){
+    this.usuarioServicio.updateUsuario(this.usuarioId,this.usuario).subscribe();
+  }*/
+
+  private deleteUsuario(usuarioId:number){
+    this.usuarioServicio.deleteUsuario(usuarioId).subscribe();
   }
 }
